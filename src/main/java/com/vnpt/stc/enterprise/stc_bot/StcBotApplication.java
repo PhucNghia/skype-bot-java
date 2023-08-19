443f89fd4d9b9c90cc5a8e3411f7458a52237ff8
nghiapn <nghiapn@vnpt-technology.vn>
2023-08-19 10:51:58 +0700

    init project

---
+++ src/main/java/com/vnpt/stc/enterprise/stc_bot/StcBotApplication.java
@@ -0,0 +1,13 @@
+package com.vnpt.stc.enterprise.stc_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StcBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StcBotApplication.class, args);
+	}
+
+}
